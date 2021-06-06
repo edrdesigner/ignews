7a9b5e9b1ef067f975367a2a5654ce01f8149d4c
Eduardo Reichert <eduardo.reichert@wssim.com.br>
2021-06-06 10:30:28 -0300

    Autencicacao com next

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,7 +1,7 @@
-import styles from './styles.module.scss';
 import { FaGithub } from 'react-icons/fa'
 import { FiX } from 'react-icons/fi'
 import { signIn, signOut, useSession } from 'next-auth/client';
+import styles from './styles.module.scss';
 
 export function SignInButton() {
   const [session]  = useSession();
