957cbf05e54f425d843da2b53b14950af772c3bf
Eduardo Reichert <eduardo.reichert@wssim.com.br>
2021-08-25 08:30:10 -0300

    mod-02:02: Validando assinatura ativa

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,7 +1,8 @@
-import { signIn, useSession } from "next-auth/client";
-import { api } from "../../services/api";
-import { getStripeJs } from "../../services/stripe-js";
-import styles from "./styles.module.scss";
+import { signIn, useSession } from 'next-auth/client';
+import { useRouter } from 'next/router';
+import { api } from '../../services/api';
+import { getStripeJs } from '../../services/stripe-js';
+import styles from './styles.module.scss';
 
 interface SubscribeButtonProps {
   priceId: string;
@@ -9,11 +10,17 @@ interface SubscribeButtonProps {
 
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
   const [session] = useSession();
+  const router = useRouter();
 
   async function handleSubscribe() {
     if (!session) {
       signIn('github');
 
+      return;
+    }
+
+    if (session.activeSubscription) {
+      router.push('/posts');
       return;
     }
 
--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -9,10 +9,43 @@ export default NextAuth({
     Providers.GitHub({
       clientId: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
-      scope: 'read:user'
+      scope: 'read:user',
     }),
   ],
   callbacks: {
+    async session(session) {
+      try {
+        const userActiveSubscription = await fauna.query(
+          q.Get(
+            q.Intersection([
+              q.Match(
+                q.Index('subscription_by_user_ref'),
+                q.Select(
+                  'ref',
+                  q.Get(
+                    q.Match(
+                      q.Index('user_by_email'),
+                      q.Casefold(session.user.email),
+                    ),
+                  ),
+                ),
+              ),
+              q.Match(q.Index('subscription_by_status'), 'active'),
+            ]),
+          ),
+        );
+
+        return {
+          ...session,
+          activeSubscription: userActiveSubscription,
+        };
+      } catch {
+        return {
+          ...session,
+          activeSubscription: null,
+        };
+      }
+    },
     async signIn(user, account, profile) {
       const { email } = user;
 
@@ -23,20 +56,12 @@ export default NextAuth({
               q.Exists(
                 q.Match(
                   q.Index('user_by_email'),
-                  q.Casefold(user.email) // case insensitive
-                )
-              )
+                  q.Casefold(user.email), // case insensitive
+                ),
+              ),
             ),
-            q.Create(
-              q.Collection('users'),
-              { data: { email } }
-            ),
-            q.Get(
-              q.Match(
-                q.Index('user_by_email'),
-                q.Casefold(user.email)
-              )
-            )
+            q.Create(q.Collection('users'), { data: { email } }),
+            q.Get(q.Match(q.Index('user_by_email'), q.Casefold(user.email))),
           ), // qIf
         );
 
@@ -45,5 +70,5 @@ export default NextAuth({
         return false;
       }
     },
-  }
-})
+  },
+});
